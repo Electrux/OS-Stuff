3aac1067b4062f78d1a9bb95d2be48277252d84d
Electrux <ckhandelwal13@gmail.com>
2017-10-02 21:51:04 +0530

    Added core/default init file as well and a very rudimentary tutorial on how to setup the config for Emacs :D

---
+++ dotfiles/dotemacs.d/core-init.el
@@ -0,0 +1,9 @@
+(require 'package) ;; You might already have this line
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (url (concat (if no-ssl "http" "https") "://melpa.org/packages/")))
+  (add-to-list 'package-archives (cons "melpa" url) t))
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize) ;; You might already have this line
