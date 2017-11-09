09fa47a0e0d4fb7101e645848ff57dff5ff4ba35
Electrux <ckhandelwal13@gmail.com>
2017-11-09 12:03:48 +0530

    Nevermind... was pretty much useless... it didn't fix the issue

--- dotfiles/dotemacs.d/init.el
+++ dotfiles/dotemacs.d/init.el
@@ -94,7 +94,6 @@
   (add-hook 'rust-mode-hook #'racer-mode)
   (add-hook 'racer-mode-hook #'eldoc-mode)
   (add-hook 'racer-mode-hook #'company-mode)
-  (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
   :config
   ;;(setq rust-format-on-save t)
   (setq rust-indent-offset 8))
