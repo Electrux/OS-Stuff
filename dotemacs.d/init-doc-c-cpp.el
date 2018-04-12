a19b8db11c181f3331270060e09c358ecd9ba241
Electrux <ckhandelwal13@gmail.com>
2018-04-12 20:34:43 +0530

    Combined init.el of document and programming ( C/C++ ) to form init-doc-c-cpp.el

---
+++ dotemacs.d/init-doc-c-cpp.el
@@ -0,0 +1,381 @@
+;;; Package --- Summary
+
+;;; Commentary:
+;;    All the settings are here.
+;;    First is the required settings for package management,
+;;    which also contains packages.
+;;    Then there is custom settings for better customization.
+
+;;; Code:
+
+
+;; Required settings for package management.
+
+(require 'package) ;; You might already have this line
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (url (concat (if no-ssl "http" "https") "://melpa.org/packages/")))
+  (add-to-list 'package-archives (cons "melpa" url) t))
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize) ;; You might already have this line
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("7527f3308a83721f9b6d50a36698baaedc79ded9f6d5bd4e9a28a22ab13b3cb1" default)))
+ '(package-selected-packages
+   (quote
+    (cargo company-ycmd eldoc-eval flycheck-rust flycheck-ycmd ycmd auctex auctex-latexmk company company-auctex exec-path-from-shell flycheck latex-extra markdown-mode markdown-mode+ markdown-preview-mode markdownfmt neotree nord-theme polymode use-package yasnippet yasnippet-snippets))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+
+
+;; Custom settings.
+
+;; Set theme.
+(load-theme 'nord)
+
+;; Fira code and its ligatures.
+(when (window-system)
+  (set-frame-font "Fira Code Retina"))
+
+(mac-auto-operator-composition-mode)
+
+;; Set default style for C/C++ language.
+(setq-default c-default-style "bsd")
+
+;; Global outlook config.
+(setq inhibit-splash-screen t); Disable splash screen
+(setq visible-bell t); Flashes on error
+
+;; Set settings for editing.
+(global-linum-mode t)
+(global-visual-line-mode 1); Proper line wrapping
+(global-hl-line-mode 1); Highlight current row
+(show-paren-mode 1); Matches parentheses and such in every mode
+(set-fringe-mode '(2 . 2)); Lower fringe because I use visual-line-mode
+;; Set indentation mode for C-esque languages.
+(setq-default c-basic-offset 8
+	      tab-width 8
+	      indent-tabs-mode t)
+
+;; Settings to disable tool bar and scroll bar from
+;; window for a clear and clean view.
+(tool-bar-mode -1)
+(toggle-scroll-bar -1)
+
+;; Disable Auto Backup feature. No more freaking '~' files!!
+(setq auto-save-default nil)
+(setq make-backup-files nil)
+
+;; Set the backup and autosave file location to /tmp .
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+
+;; Set PATH variable for emacs.
+(when (memq window-system '(mac ns x))
+  (exec-path-from-shell-initialize))
+
+;; Use either toggle-frame-fullscreen or toggle-frame-maximized as required.
+(toggle-frame-maximized)
+
+;; Package configurations.
+
+;; Dictionary.
+(setq ispell-program-name "aspell") ; could be ispell as well, depending on your preferences
+(setq ispell-dictionary "en_US-w_accents") ; this can obviously be set to any language your spell-checking program supports
+(setq ispell-extra-args '("--sug-mode=ultra"))
+
+;; Autocorrect
+(define-key ctl-x-map "\C-y"
+  #'endless/ispell-word-then-abbrev)
+
+(defun endless/simple-get-word ()
+  (car-safe (save-excursion (ispell-get-word nil))))
+
+(defun endless/ispell-word-then-abbrev (p)
+  "Call `ispell-word', then create an abbrev for it.
+With prefix P, create local abbrev. Otherwise it will
+be global.
+If there's nothing wrong with the word at point, keep
+looking for a typo until the beginning of buffer. You can
+skip typos you don't want to fix with `SPC', and you can
+abort completely with `C-g'."
+  (interactive "P")
+  (let (bef aft)
+    (save-excursion
+      (while (if (setq bef (endless/simple-get-word))
+                 ;; Word was corrected or used quit.
+                 (if (ispell-word nil 'quiet)
+                     nil ; End the loop.
+                   ;; Also end if we reach `bob'.
+                   (not (bobp)))
+               ;; If there's no word at point, keep looking
+               ;; until `bob'.
+               (not (bobp)))
+        (backward-word)
+        (backward-char))
+      (setq aft (endless/simple-get-word)))
+    (if (and aft bef (not (equal aft bef)))
+        (let ((aft (downcase aft))
+              (bef (downcase bef)))
+          (define-abbrev
+            (if p local-abbrev-table global-abbrev-table)
+            bef aft)
+          (message "\"%s\" now expands to \"%s\" %sally"
+                   bef aft (if p "loc" "glob")))
+      (user-error "No typo at or before point"))))
+
+(setq save-abbrevs 'silently)
+(setq-default abbrev-mode t)
+
+;; R mode using polymode.
+(use-package polymode
+  :init
+  (add-hook 'markdown-mode-hook 'flyspell-mode)
+  ;; MARKDOWN
+  (add-to-list 'auto-mode-alist '("\\.md" . poly-markdown-mode))
+
+  ;; R modes
+  (add-to-list 'auto-mode-alist '("\\.Snw" . poly-noweb+r-mode))
+  (add-to-list 'auto-mode-alist '("\\.Rnw" . poly-noweb+r-mode))
+  (add-to-list 'auto-mode-alist '("\\.Rmd" . poly-markdown+r-mode)))
+
+;; Setup the neotree directory viewer.
+(use-package neotree
+  :bind ([f8] . neotree-toggle)
+  :init (add-to-list 'load-path "~/")
+  :config (setq neo-smart-open t))
+
+;; Need to add a hook to the cc-mode to reset the delete key to delete last character.
+;; This fixes the tab character treatment when pressing backspace.
+(defun c-reset-del nil "Reset the delete character to default."
+  (local-set-key (kbd "DEL") 'delete-backward-char))
+(add-hook 'c-mode-common-hook 'c-reset-del)
+
+;; Need to add a hook to the rust-mode-hook for setting indent mode to tabs.
+(defun rust-indent-hook nil "Alters indentation to tabs for rust files."
+  (setq-local indent-tabs-mode t))
+(add-hook 'rust-mode-hook #'rust-indent-hook)
+
+
+;; YCMD + Company + Yasnippet + FlyCheck setup.
+
+;; YA - Snippets
+(use-package yasnippet
+  :ensure t
+  :diminish yas-minor-mode
+  :init (yas-global-mode t))
+
+;; Autocomplete
+(use-package company
+  :defer 10
+  :diminish company-mode
+  :bind (:map company-active-map
+              ("M-j" . company-select-next)
+              ("M-k" . company-select-previous))
+  :preface
+  ;; enable yasnippet everywhere
+  (defvar company-mode/enable-yas t
+    "Enable yasnippet for all backends.")
+  (defun company-mode/backend-with-yas (backend)
+    (if (or
+         (not company-mode/enable-yas)
+         (and (listp backend) (member 'company-yasnippet backend)))
+        backend
+      (append (if (consp backend) backend (list backend))
+              '(:with company-yasnippet))))
+
+  :init
+  (global-company-mode t)
+
+  :config
+  ;; no delay no autocomplete
+  (setq-default
+   company-idle-delay 0
+   company-minimum-prefix-length 2
+   company-tooltip-limit 20)
+
+  (setq-default company-backends
+		(mapcar #'company-mode/backend-with-yas company-backends)))
+
+;; Show argument list in echo area
+(use-package eldoc
+  :diminish eldoc-mode
+  :init (add-hook 'ycmd-mode-hook 'ycmd-eldoc-setup))
+
+; Code-comprehension server
+(use-package ycmd
+  :ensure t
+  :init (add-hook 'after-init-hook #'global-ycmd-mode)
+  :config
+  ;; Need absolute path in ycmd-server-command to work correctly.
+  (set-variable 'ycmd-server-command '("python3" "/Users/electrux/GitHub/ycmd/ycmd/"))
+  (set-variable 'ycmd-global-config (expand-file-name "~/GitHub/Electrux/OS-Stuff/MacOS/Dotfiles/.ycm_extra_conf.py"))
+
+  (set-variable 'ycmd-extra-conf-whitelist '("~/Programming/*" "~/GitHub/*"))
+
+  (use-package flycheck-ycmd
+    :commands (flycheck-ycmd-setup)
+    :init (add-hook 'ycmd-mode-hook 'flycheck-ycmd-setup))
+
+  (use-package company-ycmd
+    :ensure t
+    :init (company-ycmd-setup)
+    :config (add-to-list 'company-backends (company-mode/backend-with-yas 'company-ycmd))))
+
+;; LaTeX mode using AUCTex.
+(use-package auctex
+  :defer t
+  :ensure auctex
+  :init
+  (add-hook 'LaTeX-mode-hook 'turn-off-auto-fill)
+  ;; Required
+  (setq TeX-parse-self t); Enable parse on load.
+  (setq TeX-auto-save t); Enable parse on save.
+  (setq-default TeX-master nil)
+  ;; Don't use large fonts for sections.
+  (setq font-latex-fontify-sectioning 1.0)
+
+  ;; Spell check hooks
+  (add-hook 'TeX-mode-hook 'flyspell-mode); Enable Flyspell mode for TeX modes such as AUCTeX. Highlights all misspelled words.
+  ;;(add-hook 'emacs-lisp-mode-hook 'flyspell-prog-mode); Enable Flyspell program mode for emacs lisp mode, which highlights all misspelled words in comments and strings.
+  (add-hook 'TeX-mode-hook
+	    (lambda () (TeX-fold-mode 1))); Automatically activate TeX-fold-mode.
+  ;; Enable LaTeX extra mode
+  (add-hook 'TeX-mode-hook #'latex-extra-mode)
+
+  (setq LaTeX-babel-hyphen nil); Disable language-specific hyphen insertion.
+
+  ;; I shall only use PDF mode
+  (setq TeX-PDF-mode t); PDF mode (rather than DVI-mode)
+
+  :config
+  ;; " expands into csquotes macros (for this to work babel must be loaded after csquotes).
+  (setq LaTeX-csquotes-close-quote "}"
+	LaTeX-csquotes-open-quote "\\enquote{")
+
+  ;; LaTeX-math-mode http://www.gnu.org/s/auctex/manual/auctex/Mathematics.html
+  (add-hook 'TeX-mode-hook 'LaTeX-math-mode)
+
+  ;; RefTeX
+  ;; Turn on RefTeX for AUCTeX http://www.gnu.org/s/auctex/manual/reftex/reftex_5.html
+  (add-hook 'TeX-mode-hook 'turn-on-reftex)
+
+  ;; Autosave
+  (setq TeX-auto-save t))
+
+;; RefTex config.
+(use-package reftex
+  :config
+  (eval-after-load 'reftex-vars; Is this construct really needed?
+    '(progn
+       (setq reftex-cite-prompt-optional-args t); Prompt for empty optional arguments in cite macros.
+       ;; Make RefTeX interact with AUCTeX, http://www.gnu.org/s/auctex/manual/reftex/AUCTeX_002dRefTeX-Interface.html
+       (setq reftex-plug-into-AUCTeX t)
+       ;; So that RefTeX also recognizes \addbibresource. Note that you
+       ;; can't use $HOME in path for \addbibresource but that "~"
+       ;; works.
+       (setq reftex-bibliography-commands '("bibliography" "nobibliography" "addbibresource"))
+					;     (setq reftex-default-bibliography '("UNCOMMENT LINE AND INSERT PATH TO YOUR BIBLIOGRAPHY HERE")); So that RefTeX in Org-mode knows bibliography
+       (setcdr (assoc 'caption reftex-default-context-regexps) "\\\\\\(rot\\|sub\\)?caption\\*?[[{]"); Recognize \subcaptions, e.g. reftex-citation
+       (setq reftex-cite-format; Get ReTeX with biblatex, see https://tex.stackexchange.com/questions/31966/setting-up-reftex-with-biblatex-citation-commands/31992#31992
+	     '((?t . "\\textcite[]{%l}")
+	       (?a . "\\autocite[]{%l}")
+	       (?c . "\\cite[]{%l}")
+	       (?s . "\\smartcite[]{%l}")
+	       (?f . "\\footcite[]{%l}")
+	       (?n . "\\nocite{%l}")
+	       (?b . "\\blockcquote[]{%l}{}")))))
+
+  ;; Fontification (remove unnecessary entries as you notice them)
+  (setq font-latex-match-reference-keywords
+	'(
+	  ;; biblatex
+	  ("printbibliography" "[{")
+	  ("addbibresource" "[{")
+	  ;; Standard commands
+	  ;; ("cite" "[{")
+	  ("Cite" "[{")
+	  ("parencite" "[{")
+	  ("Parencite" "[{")
+	  ("footcite" "[{")
+	  ("footcitetext" "[{")
+	  ;; Style-specific commands
+	  ("textcite" "[{")
+	  ("Textcite" "[{")
+	  ("smartcite" "[{")
+	  ("Smartcite" "[{")
+	  ("cite*" "[{")
+	  ("parencite*" "[{")
+	  ("supercite" "[{")
+	  ;; Qualified citation lists
+	  ("cites" "[{")
+	  ("Cites" "[{")
+	  ("parencites" "[{")
+	  ("Parencites" "[{")
+	  ("footcites" "[{")
+	  ("footcitetexts" "[{")
+	  ("smartcites" "[{")
+	  ("Smartcites" "[{")
+	  ("textcites" "[{")
+	  ("Textcites" "[{")
+	  ("supercites" "[{")
+	  ;; Style-independent commands
+	  ("autocite" "[{")
+	  ("Autocite" "[{")
+	  ("autocite*" "[{")
+	  ("Autocite*" "[{")
+	  ("autocites" "[{")
+	  ("Autocites" "[{")
+	  ;; Text commands
+	  ("citeauthor" "[{")
+	  ("Citeauthor" "[{")
+	  ("citetitle" "[{")
+	  ("citetitle*" "[{")
+	  ("citeyear" "[{")
+	  ("citedate" "[{")
+	  ("citeurl" "[{")
+	  ;; Special commands
+	  ("fullcite" "[{")))
+
+  (setq font-latex-match-textual-keywords
+	'(
+	  ;; biblatex brackets
+	  ("parentext" "{")
+	  ("brackettext" "{")
+	  ("hybridblockquote" "[{")
+	  ;; Auxiliary Commands
+	  ("textelp" "{")
+	  ("textelp*" "{")
+	  ("textins" "{")
+	  ("textins*" "{")
+	  ;; supcaption
+	  ("subcaption" "[{")))
+
+  (setq font-latex-match-variable-keywords
+	'(
+	  ;; amsmath
+	  ("numberwithin" "{")
+	  ;; enumitem
+	  ("setlist" "[{")
+	  ("setlist*" "[{")
+	  ("newlist" "{")
+	  ("renewlist" "{")
+	  ("setlistdepth" "{")
+	  ("restartlist" "{"))))
+
+(provide 'init)
+;;; init.el ends here
