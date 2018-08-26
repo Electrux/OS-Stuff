536288637a09f9fc542c5434540905d5012eebbf
Electrux <ElectruxRedsworth@gmail.com>
2018-08-26 18:28:12 +0530

    Changed color scheme to pinkish, Dreamy Sword Girl as wallpaper

---
+++ Arch/Dotfiles/dotconfig/bspwm/scripts/colors.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# border
+export BORDER="#e68ef5"
+export BORDERNORM="#b980ca"
+
+# special
+export foreground="#e3cac6"
+export background="#150d0b"
+export cursorColor="#e3cac6"
+
+# black
+export color0="#150d0b"
+export color8="#9e8d8a"
+
+# red
+export color1="#7878c1"
+export color9="#7878c1"
+
+# green
+export color2="#9076a2"
+export color10="#9076a2"
+
+# yellow
+export color3="#bd7ca0"
+export color11="#bd7ca0"
+
+# blue
+export color4="#7f81bc"
+export color12="#7f81bc"
+
+# magenta
+export color5="#a68ba7"
+export color13="#a68ba7"
+
+# cyan
+export color6="#ad8fb1"
+export color14="#ad8fb1"
+
+# white
+export color7="#e3cac6"
+export color15="#e3cac6"
