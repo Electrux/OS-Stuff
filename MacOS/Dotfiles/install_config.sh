25484a4326601fdbf1d195b718b5c931d3bc5621
Electrux <ElectruxRedsworth@gmail.com>
2018-10-20 19:47:40 +0530

    Added mpd and neovim auto config via install_config.sh

--- MacOS/Dotfiles/install_config.sh
+++ MacOS/Dotfiles/install_config.sh
@@ -2,8 +2,11 @@
 
 script_dir=$(dirname "$0")
 
-mkdir -p ~/.config/ ~/.mpd
+mkdir -p ~/.config/nvim ~/.mpd
 
 ln -sf ${script_dir}/.{zshrc,vimrc,spacemacs,ncmpcpp} ~/
 ln -sf ${script_dir}/.mpd/mpd.conf ~/.mpd/mpd.conf
 ln -sf ${script_dir}/.config/ranger ~/.config/
+
+# neovim
+ln -sf ${script_dir}/.vimrc ~/.config/nvim/init.vim
